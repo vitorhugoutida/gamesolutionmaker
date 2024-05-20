c4366aef528aac21f2cdb09b67109ce8de5baef4
vitorhugoutida <vitorhugoutida@gmail.com>
2024-05-20 17:28:16 -0300

    feat: Acabei o formulario API

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let menu = document.getElementById("menu")
+// let header = document.getElementById("menu-header")
 let iconeBarras = document.getElementById("icone-barras")
 let iconeX = document.getElementById("icone-x")
 
@@ -25,6 +26,7 @@ function abrirFecharMenu() {
         // fechar o menu
 
         menu.classList.add("menu-fechado")
+        
 
         // esconder icone x
 
@@ -41,3 +43,53 @@ window.onresize = () => {
     iconeX.style.display = "inline"
     iconeBarras.style.display = "none"
 } 
+
+// ------------------------------------------------------------------------------------
+
+const solicitarOrcamento = (event) => {
+    // Pegar valores dos inputs
+    let valorNome = document.getElementById("campo-nome").value
+    let valorEmail = document.getElementById("campo-email").value
+    let valorTelefone = document.getElementById("campo-telefone").value
+    let valorDescricao = document.getElementById("campo-descricao").value
+
+    // Organizar objeto com os valores
+
+    let dadosForm = {
+        nome: valorNome,
+        email: valorEmail,
+        telefone: valorTelefone,
+        descricao: valorDescricao
+    }
+    // enviar requisicao para a api
+    // 127.0.0.1 = localhost
+    // Metodo HTTP POST - Create -> Cadastrar ou criar
+
+    fetch("http://localhost:3000/solicitacoes", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+
+        }, 
+        body: JSON.stringify(dadosForm)
+
+    })
+
+    .then(resposta => {
+        console.log(resposta)
+
+        // Limpar os campos (inputs)
+        document.querySelector("#contato-form form").reset()
+        
+    // Mostrar alert com msg de sucesso
+    alert("Solicitação cadastrada")
+
+})
+// Caso ERRO - alert com msg erro
+    .catch(erro => {
+        console.error(erro)
+        alert("Erro na requisição")
+    })
+
+    event.preventDefault()
+}
